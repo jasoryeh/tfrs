1ca7313eb866d56d51317a6cc4f3e3dbf2da3c24
Jason Ho <git@hogt.me>
2023-03-28 11:37:59 -0700

    enable cors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 const tfrs = require('../index');
+
+const corsHeaders = {
+	"Access-Control-Allow-Origin": "*"
+}
+
+function getHeaders(otherHeaders = {}) {
+	return {
+		...corsHeaders,
+		...otherHeaders
+	}
+}
 
 export default {
 	async fetch(request, env, ctx) {
@@ -16,15 +18,15 @@ export default {
 		if (tfrID == "") {
 			let tfrlist = await tfrs.list();
 			return new Response(JSON.stringify(tfrlist, null, 4), {
-				headers: {
+				headers: getHeaders({
 					"content-type": "application/json;charset=UTF-8"
-				}
+				})
 			});
 		} else {
 			return new Response(JSON.stringify(await tfrs.fetch(tfrID), null, 4), {
-				headers: {
+				headers: getHeaders({
 					"content-type": "application/json;charset=UTF-8"
-				}
+				})
 			});
 		}
 	},
