b810f6ad77912ac642bd364558d7bbe748551298
Jason Ho <git@hogt.me>
2023-03-28 16:18:45 -0700

    cache tfr responses to reduce requests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const tfrs = require('../index');
+const CACHE_TIME = 1000 * 60 * 10;
 
 const corsHeaders = {
 	"Access-Control-Allow-Origin": "*"
@@ -11,8 +12,24 @@ function getHeaders(otherHeaders = {}) {
 	}
 }
 
+function cacheKey(key) {
+	return "cache_" + key;
+}
+
+async function fromCacheOrElse(cache_namespace, key, populate) {
+	let cache_id = cacheKey(key);
+	var data = cache_namespace.get(cache_id);
+	if (data == null) {
+		data = await populate();
+		await cache_namespace.put(cache_id, data, { expiration: parseInt((CACHE_TIME + new Date().getTime()) / 1000) });
+	}
+	return data;
+}
+
 export default {
 	async fetch(request, env, ctx) {
+		const TFR_CACHE = env.TFR_CACHE;
+
 		let url = new URL(request.url);
 		let tfrID = url.pathname.replace('/', '');
 		if (tfrID == "") {
@@ -23,7 +40,9 @@ export default {
 				})
 			});
 		} else {
-			return new Response(JSON.stringify(await tfrs.fetch(tfrID), null, 4), {
+			return new Response(await fromCacheOrElse(TFR_CACHE, "tfr_" + tfrID, async () => {
+				return JSON.stringify(await tfrs.fetch(tfrID), null, 4);
+			}), {
 				headers: getHeaders({
 					"content-type": "application/json;charset=UTF-8"
 				})
